5b3585e1bb8046b9402e3e5c885758f75af25314
samivicasol <sami@vicasol.com>
2026-05-29 11:57:50 +0200

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-DATA_DIR = "/data"
+DATA_DIR = os.getenv("DATA_DIR", "/tmp/data")
 CHROMA_PATH = "vectordb"
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2" # Modelo de embeddings local
 LLM_MODEL = "llama-3.3-70b-versatile" # Modelo de Groq
